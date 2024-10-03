560485c0a4a6212bf7ed7e04466279679dbee4f5
WYstudio <wystudio001@qq.com>
2024-10-03 17:53:43 +0800

    New: Add Activity and Updata some Files

--- app/src/main/java/xyz/wystudio/qistudio/program/autobackup/App.java
+++ app/src/main/java/xyz/wystudio/qistudio/program/autobackup/App.java
@@ -1,5 +1,7 @@
 package xyz.wystudio.qistudio.program.autobackup;
 
+import android.widget.Toast;
+
 import com.rapid.framework.program.android.DvmPluginDescriptor;
 import com.rapid.framework.program.android.app.AndroidPlugin;
 
@@ -9,12 +11,11 @@ public class App extends AndroidPlugin {
     @Override
     public void onInit(DvmPluginDescriptor descriptor) {
         super.onInit(descriptor);
-
     }
 
     @Override
     public void onInstall() {
-
+        Toast.makeText(getApplicationContext(),"备份插件安装成功",Toast.LENGTH_LONG).show();
     }
 
     @Override
